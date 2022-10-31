8b417ebb6c2bb044436655c89e7e8e9ca06df3d2
Wanzi <Mr.Wanzi@outlook.com>
2022-10-31 15:36:06 +0800

    first test

---
+++ diy-part1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#===============================================
+# Description: DIY script
+# File name: diy-part1.sh
+# Lisence: MIT
+#===============================================
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git https://github.com/sirpdboy/sirpdboy-package.git' >>feeds.conf.default
+echo 'src-git node https://github.com/nxhack/openwrt-node-packages.git' >>feeds.conf.default
